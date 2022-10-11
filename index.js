28baf16c2e82081254bf2e35da927e196729b420
Pawan Singh <89253918+pawan-singh-au26@users.noreply.github.com>
2022-10-11 20:21:08 +0530

    button ripal effect

--- index.js
+++ index.js
@@ -1,43 +1,8 @@
-const num1= Math.ceil(Math.random()*10);
-const num2= Math.ceil(Math.random()*10);
+const btnEl=document.querySelector(".btn");
+btnEl.addEventListener("mouseover", (event) => {
+    const x = event.pageX - btnEl.offsetLeft;
+    const y = event.pageY - btnEl.offsetTop;
 
-const questionEl=document.getElementById("question");
-
-const inputEl=document.getElementById("input")
-
-const formEl=document.getElementById("form");
-
-const scoreEl=document.getElementById("score");
-
-let score =JSON.parse(localStorage.getItem("score"));
-
-if(!score){
-    score=0;
-}
-
-scoreEl.innerHTML=`score: ${score}`
-
-questionEl.innerHTML=`What is ${num1} multiply by ${num2} ?`
- 
-const correctAns=num1*num2;
-
-formEl.addEventListener("submit",()=>{
-    const userAns=+inputEl.value
-    if (userAns===correctAns){
-        score++;
-        updatedLocalStorege()
-    }else{
-        score--;
-        updatedLocalStorege()
-    }    
-
-    }
-
-);
-function updatedLocalStorege(){
-    localStorage.setItem("score",JSON.stringify(score))
-}
-
-
-
-
+    btnEl.style.setProperty("--xPos", x + "px")
+    btnEl.style.setProperty("--yPos", y + "px")
+});
